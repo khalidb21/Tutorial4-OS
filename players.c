add2cdc658f155ed2a6a7569f6fb3cc6a2f60adb
Alexy Pichette <137420927+Pichettea@users.noreply.github.com>
2026-02-12 09:34:45 -0500

    Implement player management functions in players.c

---
+++ players.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "players.h"
+
+bool player_exists(player *players, int num_players, const char *name) {
+    for (int i = 0; i < num_players; i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void update_score(player *players, int num_players, const char *name, int points) {
+    for (int i = 0; i < num_players; i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            players[i].score += points;
+            return;
+        }
+    }
+}
+
+void highest_score(player *players, int num_players) {
+    int best = 0;
+
+    for (int i = 1; i < num_players; i++) {
+        if (players[i].score > players[best].score) {
+            best = i;
+        }
+    }
+
+    printf("\nWinner: %s with $%d\n", players[best].name, players[best].score);
+}
